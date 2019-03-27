d7c0c03bcece67286883cf6dee83b2cb3bdb8249
wangyunhao993 <993120814@qq.com>
2019-03-27 12:48:36 +0800

    加载主页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+// use
+Vue.use(VueRouter)
+// 全局样式
+import './assets/statics/site/css/style.css'
 Vue.config.productionTip = false
+
+// 导入组件
+import index from './components/index.vue'
+// 规则
+let routes =[
+  {
+    path:'/',
+    component:index
+  },
+  {
+    path:'/index',
+    component:index
+  }
+]
+
+let router = new VueRouter({
+  routes
+})
+
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
